1ac3d3a5274a5160c0b251628cd12c1bae0a7f9f
ailgiP <41807528@qq.com>
2017-10-10 01:42:57 +0800

    完成了查看订单页面的交互，但是标签属性的显示有bug

--- pages/orders/orders.js
+++ pages/orders/orders.js
@@ -1,171 +1,41 @@
+// pages/order/order.js
+const app = getApp();
+const API = getApp().globalData.API;
+
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    orders:[
-      {
-        id: 1234,
-        status: "已完成",
-        products: [
-          {
-            id: 1,
-            title: "卖萌猪崽",
-            size:[
-              {
-                id: 1,
-                name: "1kg",
-                value: "1"
-              },
-              {
-                id: 2,
-                name: "A类",
-                value: "A"
-              }
-            ],
-            quantity: 2,
-            price: 10.11
-          },
-          {
-            id: 2,
-            title: "大头猪崽",
-            size: [
-              {
-                id: 1,
-                name: "5kg",
-                value: "5"
-              },
-              {
-                id: 2,
-                name: "B类",
-                value: "B"
-              }
-            ],
-            quantity: 1,
-            price: 12.13
-          },
-          {
-            id: 2,
-            title: "大头猪崽",
-            size: [
-              {
-                id: 1,
-                name: "5kg",
-                value: "5"
-              },
-              {
-                id: 2,
-                name: "B类",
-                value: "B"
-              }
-            ],
-            quantity: 1,
-            price: 12.13
-          },
-        ],
-        count: 99.99,
-        time: "2017-9-21 16:18"
+   orders: []
+  },
+  
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    const that = this;
+    // 第一步，请求获取个人所有订单
+    wx.request({
+      url: API + "/api/my/orders",
+      header: {
+        Authorization: 'Bearer' + app.globalData.token
       },
-      {
-        id: 1234,
-        status: "已完成",
-        products: [
-          {
-            id: 1,
-            title: "卖萌猪崽",
-            size: [
-              {
-                id: 1,
-                name: "1kg",
-                value: "1"
-              },
-              {
-                id: 2,
-                name: "A类",
-                value: "A"
-              }
-            ],
-            quantity: 2,
-            price: 10.11
-          },
-          {
-            id: 2,
-            title: "大头猪崽",
-            size: [
-              {
-                id: 1,
-                name: "5kg",
-                value: "5"
-              },
-              {
-                id: 2,
-                name: "B类",
-                value: "B"
-              }
-            ],
-            quantity: 1,
-            price: 12.13
-          },
-        ],
-        count: 99.99,
-        time: "2017-9-21 16:18"
-      },
-      {
-        id: 1234,
-        status: "已完成",
-        products: [
-          {
-            id: 1,
-            title: "卖萌猪崽",
-            size: [
-              {
-                id: 1,
-                name: "1kg",
-                value: "1"
-              },
-              {
-                id: 2,
-                name: "A类",
-                value: "A"
-              },
-              {
-                id: 1,
-                name: "1kg",
-                value: "1"
-              },
-              {
-                id: 2,
-                name: "A类",
-                value: "A"
-              }
-            ],
-            quantity: 2,
-            price: 10.11
-          },
-          {
-            id: 2,
-            title: "大头猪崽",
-            size: [
-              {
-                id: 1,
-                name: "5kg",
-                value: "5"
-              },
-              {
-                id: 2,
-                name: "B类",
-                value: "B"
-              }
-            ],
-            quantity: 1,
-            price: 12.13
-          },
-        ],
-        count: 99.99,
-        time: "2017-9-21 16:18"
+      success: function (res) {
+        if(res.data.success){
+          const orders = res.data.data.content;
+          that.setData({
+            orders: orders
+          })
+        }
+        console.log(that.data.orders)
       }
-    ]
+    })
+    
+
+
   },
 
   goToDetail: function(e){
